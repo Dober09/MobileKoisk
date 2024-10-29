60f87080b24231dca6307de47b184f46082724a3
Aishacmela <aishacmela@gmail.com>
2024-10-29 15:19:46 +0200

    ui(LoginPage) added loginpage layout

--- MobileKoisk/App.xaml.cs
+++ MobileKoisk/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MobileKoisk
+﻿using MobileKoisk.View;
+
+namespace MobileKoisk
 {
     public partial class App : Application
     {
@@ -6,7 +8,8 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new LoginPage();
+            
         }
     }
 }
