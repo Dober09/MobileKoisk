fe8283922e099ea5c2c5b6d46c877820e5876053
Aishacmela <aishacmela@gmail.com>
2024-12-10 16:11:09 +0200

    Added method to calculate the total price on the basketpage

--- MobileKoisk/ViewModel/BasketPageViewModel.cs
+++ MobileKoisk/ViewModel/BasketPageViewModel.cs
@@ -1,36 +1,72 @@
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 using System.Windows.Input;
-
 using MobileKoisk.Models;
-using MobileKoisk.Services;
-using MobileKoisk.View;
-
-
-
 
 namespace MobileKoisk.ViewModel
 {
-	public class BasketPageViewModel
+	public class BasketPageViewModel : INotifyPropertyChanged
 	{
-	
+		private decimal _totalPrice;
 
 		public ICommand ProceedToPaymentCommand { get; }
 
 		public ObservableCollection<BasketItem> BasketItems { get; set; }
 
-		public BasketPageViewModel()
+		public decimal TotalPrice
 		{
-			
-
-			BasketItems = new ObservableCollection<BasketItem>
+			get => _totalPrice;
+			set
 			{
-			   new BasketItem { ImageSource = "coffee.png", ProductName = "Coffee", Quantity = 1, Price = 19.99M,  ProductSize = "750 ml" },
-			   new BasketItem { ImageSource = "selatisugar.png", ProductName = "Sugar", Quantity = 1, Price = 9.99M, ProductSize = "1 kg" },
-			   new BasketItem { ImageSource = "cremora.png", ProductName = "Milk", Quantity = 2, Price = 29.99M, ProductSize = "500 ml" }
-
-			};
+				if (_totalPrice != value)
+				{
+					_totalPrice = value;
+					OnPropertyChanged(nameof(TotalPrice));
+				}
+			}
 		}
 
-		
+		public BasketPageViewModel()
+		{
+			// Initialize the basket items
+			BasketItems = new ObservableCollection<BasketItem>
+			{
+				new BasketItem { ImageSource = "coffee.png", ProductName = "Coffee", Quantity = 1, Price = 19.99M, ProductSize = "750 ml" },
+				new BasketItem { ImageSource = "selatisugar.png", ProductName = "Sugar", Quantity = 1, Price = 9.99M, ProductSize = "1 kg" },
+				new BasketItem { ImageSource = "cremora.png", ProductName = "Milk", Quantity = 2, Price = 29.99M, ProductSize = "500 ml" }
+			};
+
+			// Attach property change notifications for basket items
+			foreach (var item in BasketItems)
+			{
+				item.PropertyChanged += OnBasketItemChanged;
+			}
+
+			// Update the total price
+			UpdateTotalPrice();
+		}
+
+		private void OnBasketItemChanged(object sender, PropertyChangedEventArgs e)
+		{
+			if (e.PropertyName == nameof(BasketItem.Quantity))
+			{
+				UpdateTotalPrice();
+			}
+		}
+
+		private void UpdateTotalPrice()
+		{
+			// Calculate the total price by summing up the price of each item multiplied by its quantity
+			TotalPrice = BasketItems.Sum(item => item.Price * item.Quantity);
+		}
+
+		// Correctly implement the INotifyPropertyChanged interface
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
 	}
 }
