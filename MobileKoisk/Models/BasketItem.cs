468209d3eb470394f97d7f42caa146876b6249b5
Aishacmela <aishacmela@gmail.com>
2024-12-10 15:23:11 +0200

    added quantity counter on the basketitem class and bind to the basketpage

--- MobileKoisk/Models/BasketItem.cs
+++ MobileKoisk/Models/BasketItem.cs
@@ -1,17 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Windows.Input;
 
 namespace MobileKoisk.Models
 {
-    public class BasketItem
-    {
-        public string? ImageSource { get; set; }
+	public class BasketItem : INotifyPropertyChanged
+	{
+		// Properties
+		public string? ImageSource { get; set; }
 		public string? ProductName { get; set; }
-		public int Quantity { get; set; }
 		public decimal Price { get; set; }
 		public string ProductSize { get; set; }
+
+		private int _quantity;
+		public int Quantity
+		{
+			get => _quantity;
+			set
+			{
+				if (_quantity != value)
+				{
+					_quantity = value;
+					OnPropertyChanged(nameof(Quantity));
+				}
+			}
+		}
+
+		// Commands
+		public ICommand IncreaseQuantityCommand { get; }
+		public ICommand DecreaseQuantityCommand { get; }
+
+		// Constructor
+		public BasketItem()
+		{
+			IncreaseQuantityCommand = new Command(() => IncreaseQuantity());
+			DecreaseQuantityCommand = new Command(() => DecreaseQuantity());
+		}
+
+		// Methods
+		private void IncreaseQuantity()
+		{
+			Quantity++;
+		}
+
+		private void DecreaseQuantity()
+		{
+			if (Quantity > 0)
+			{
+				Quantity--;
+			}
+		}
+
+		// PropertyChanged Implementation
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
 	}
 }
