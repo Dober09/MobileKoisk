27b613d83cb8073a1aad0eab1495cef72e55152e
Aishacmela <aishacmela@gmail.com>
2024-12-10 12:07:05 +0200

    (layout) updated basketpage layout

--- MobileKoisk/Models/BasketItem.cs
+++ MobileKoisk/Models/BasketItem.cs
@@ -11,6 +11,7 @@ namespace MobileKoisk.Models
         public string? ImageSource { get; set; }
 		public string? ProductName { get; set; }
 		public int Quantity { get; set; }
-		public decimal Price { get; set; }	
+		public decimal Price { get; set; }
+		public string ProductSize { get; set; }
 	}
 }
--- MobileKoisk/ViewModel/BasketPageViewModel.cs
+++ MobileKoisk/ViewModel/BasketPageViewModel.cs
@@ -24,9 +24,9 @@ namespace MobileKoisk.ViewModel
 
 			BasketItems = new ObservableCollection<BasketItem>
 			{
-			   new BasketItem { ImageSource = "coffee.png", ProductName = "Coffee", Quantity = 1, Price = 19.99M },
-			   new BasketItem { ImageSource = "selatisugar.png", ProductName = "Sugar", Quantity = 1, Price = 9.99M },
-			   new BasketItem { ImageSource = "cremora.png", ProductName = "Milk", Quantity = 2, Price = 29.99M }
+			   new BasketItem { ImageSource = "coffee.png", ProductName = "Coffee", Quantity = 1, Price = 19.99M,  ProductSize = "750 ml" },
+			   new BasketItem { ImageSource = "selatisugar.png", ProductName = "Sugar", Quantity = 1, Price = 9.99M, ProductSize = "1 kg" },
+			   new BasketItem { ImageSource = "cremora.png", ProductName = "Milk", Quantity = 2, Price = 29.99M, ProductSize = "500 ml" }
 			};
 		}
 
